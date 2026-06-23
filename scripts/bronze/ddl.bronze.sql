705553790f9260921fc39ee76f05d92316bd6b5d
Công Nguyễn <ntkenc3005@gmail.com>
2026-06-23 17:55:43 +0700

    Update ddl.bronze.sql

--- scripts/bronze/ddl.bronze.sql
+++ scripts/bronze/ddl.bronze.sql
@@ -10,65 +10,98 @@ Script Purpose
 */
 
 
-IF OBJECT_ID ('bronze.crm_cust_info', 'U') is not null 
-	DROP TABLE bronze.crm_cust_info;
-CREATE TABLE bronze.crm_cust_info( 
-	cst_id				int,
-	cst_key				nvarchar(50) ,
-	cst_firstname		nvarchar(50),
-	cst_lastname		nvarchar(50),
-	cst_marital_status	nvarchar(50),
-	cst_gndr			nvarchar(50),
-	cst_create_date		date
+Create schema bronze;
+go
+IF OBJECT_ID ('bronze.account', 'U') is not null 
+	DROP TABLE bronze.account;
+Create Table bronze.account( 
+	account_id		nvarchar(50), 
+	district_id		nvarchar(50),
+	frequency		nvarchar(50),
+	date			nvarchar(50)
 );
 
-IF OBJECT_ID ('bronze.crm_prd_info', 'U') is not null 
-	DROP TABLE bronze.crm_prd_info;
-CREATE TABLE bronze.crm_prd_info( 
-	prd_id			int,
-	prd_key			nvarchar(50),
-	prd_nm			nvarchar(50),
-	prd_cost		int,
-	prd_line		nvarchar(50),
-	prd_start_dt	datetime,
-	prd_end_dt		datetime
+IF OBJECT_ID ('bronze.card', 'U') is not null 
+	DROP TABLE bronze.card;
+Create table bronze.card(
+	card_id			nvarchar(50),
+	disp_id			nvarchar(50),
+	type			nvarchar(50),
+	issued			nvarchar(50)
 );
 
 
-IF OBJECT_ID ('bronze.crm_sales_detaisl', 'U') is not null 
-	DROP TABLE bronze.crm_sales_detaisl;
-CREATE TABLE bronze.crm_sales_details( 
-	sls_ord_num		nvarchar(50),
-	sls_prd_key		nvarchar(50),
-	sls_cust_id		int,
-	sls_order_dt	int,
-	sls_ship_dt		int,
-	sls_due_dt		int,
-	sls_sales		int,
-	sls_quantity	int,
-	sls_price		int
-);
+IF OBJECT_ID ('bronze.client', 'U') is not null 
+	DROP TABLE bronze.client;
+Create table bronze.client( 
+	client_id		varchar(50),
+	birth_number	nvarchar(50),
+	district_id		varchar(50)
+)
 
-IF OBJECT_ID ('bronze.erp_cust_az12', 'U') is not null 
-	DROP TABLE bronze.erp_cust_az12;
-CREATE TABLE bronze.erp_cust_az12( 
-	CID		nvarchar(50),
-	BDATE	datetime,
-	GEN		nvarchar(50)
-);
+IF OBJECT_ID ('bronze.disp', 'U') is not null 
+	DROP TABLE bronze.disp;
+Create table bronze.disp(
+	disp_id			nvarchar(50),
+	client_id		nvarchar(50), 
+	account_id		nvarchar(50),
+	type			varchar(50)
+)
 
-IF OBJECT_ID ('bronze.erp_loc_a101', 'U') is not null 
-	DROP TABLE bronze.erp_loc_a101;
-CREATE TABLE bronze.erp_loc_a101( 
-	CID		nvarchar(50),
-	CNTRY	nvarchar(50)
-);
+IF OBJECT_ID ('bronze.district', 'U') is not null 
+	DROP TABLE bronze.district;
+Create table bronze.district(
+	A1				nvarchar(50),
+	A2				nvarchar(50),
+	A3				nvarchar(50),
+	A4				nvarchar(50),
+	A5				nvarchar(50), 
+	A6				nvarchar(50),
+	A7				nvarchar(50), 
+	A8				nvarchar(50), 
+	A9				nvarchar(50), 
+	A10				nvarchar(50),
+	A11				nvarchar(50), 
+	A12				nvarchar(50),
+	A13				nvarchar(50),
+	A14				nvarchar(50),
+	A15				nvarchar(50),
+	A16				nvarchar(50)
+)
 
-IF OBJECT_ID ('bronze.erp_px_cat_g1v2', 'U') is not null 
-	DROP TABLE bronze.erp_px_cat_g1v2;
-CREATE TABLE bronze.erp_px_cat_g1v2( 
-	ID				nvarchar(50),
-	CAT				nvarchar(50),
-	SUBCAT			nvarchar(50),
-	MAINTENANCE		nvarchar(50)
-);
+IF OBJECT_ID ('bronze.loan', 'U') is not null 
+	DROP TABLE bronze.loan;
+Create table bronze.loan(
+	loan_id			nvarchar(50),
+	account_id		nvarchar(50),
+	date			date,
+	amount			nvarchar(50),
+	duration		nvarchar(50),
+	payments		nvarchar(50)
+)
+
+IF OBJECT_ID ('bronze.orders', 'U') is not null 
+	DROP TABLE bronze.orders;
+Create table bronze.orders(
+	order_id		nvarchar(50), 
+	account_id		nvarchar(50), 
+	bank_to			varchar(10),
+	account_to		nvarchar(50), 
+	amount			nvarchar(50),
+	k_symbol		varchar(50)
+)
+
+IF OBJECT_ID ('bronze.trans', 'U') is not null 
+	DROP TABLE bronze.trans;
+Create table bronze.trans(
+	trans_id		nvarchar(50), 
+	account_id		nvarchar(50), 
+	date			nvarchar(50),
+	type			varchar(50),
+	operation		varchar(50),
+	amount			nvarchar(50), 
+	balance			nvarchar(50), 
+	k_symbol		nvarchar(50),
+	bank			varchar(10),
+	account			nvarchar(50)
+) 
